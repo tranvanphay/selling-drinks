6e536a7feae52c0286cd57285aaee0f48a08635a
tranvanphay <42576241+tranvanphay@users.noreply.github.com>
2018-11-09 22:45:10 +0700

    đăng nhập facebook...(chỉ dành cho người dùng)

--- app/src/main/java/com/example/lenovo/duan1/LoginActivity.java
+++ app/src/main/java/com/example/lenovo/duan1/LoginActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -18,6 +19,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.lenovo.duan1.model.AccountNguoiDung;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -30,6 +36,8 @@ public class LoginActivity extends AppCompatActivity {
  TextView tvDangKy,tvQuenMK;
  AccountNguoiDung accountNguoiDung;
  DatabaseReference mData;
+ private CallbackManager mCallbackManager;
+ public static final String TAG="FACELOG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -142,8 +150,40 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+//Facebook Login
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = findViewById(R.id.login_facebook_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent intent=new Intent(LoginActivity.this,NguoiDungActivity.class);
+                startActivity(intent);
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                //handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
 
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
 
     }
 }
