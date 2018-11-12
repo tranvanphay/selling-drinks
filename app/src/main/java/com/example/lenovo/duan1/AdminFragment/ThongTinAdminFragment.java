86bd0b3cff5a0210484af78bc73f78566adf7101
tranvanphay <42576241+tranvanphay@users.noreply.github.com>
2018-11-12 22:34:44 +0700

    (test bên ThongTinAdminFragment) lấy link ném vô database, sử dụng thư viện picaso móc về lại android...

--- app/src/main/java/com/example/lenovo/duan1/AdminFragment/ThongTinAdminFragment.java
+++ app/src/main/java/com/example/lenovo/duan1/AdminFragment/ThongTinAdminFragment.java
@@ -8,26 +8,38 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.lenovo.duan1.Adapter.HinhAnhAdapter;
 import com.example.lenovo.duan1.R;
+import com.example.lenovo.duan1.model.TestStorage;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import static android.app.Activity.RESULT_OK;
@@ -38,7 +50,11 @@ import static android.app.Activity.RESULT_OK;
 public class ThongTinAdminFragment extends Fragment {
  ImageView imv;
  Button bt_upload;
+    ListView lv_hinhAnh;
+ DatabaseReference mData = FirebaseDatabase.getInstance().getReference();
     FirebaseStorage storage = FirebaseStorage.getInstance();
+    ArrayList<TestStorage> dsh=new ArrayList<TestStorage>();
+    HinhAnhAdapter adapter;
 
 
     public ThongTinAdminFragment() {
@@ -52,9 +68,14 @@ public class ThongTinAdminFragment extends Fragment {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.test_storage,container,false);
         final StorageReference storageRef = storage.getReferenceFromUrl("gs://duan1-ac840.appspot.com");
-
+        final EditText edtTenHinh=view.findViewById(R.id.ettenHinh);
+        lv_hinhAnh=view.findViewById(R.id.listHinh);
+        adapter=new HinhAnhAdapter(getActivity(),dsh);
+        lv_hinhAnh.setAdapter(adapter);
+        loadData();
         imv=view.findViewById(R.id.imv_getImage);
         bt_upload=view.findViewById(R.id.btUpload);
+        mData=FirebaseDatabase.getInstance().getReference();
         imv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -90,6 +111,8 @@ public class ThongTinAdminFragment extends Fragment {
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                         // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
                         // ...
+                        edtTenHinh.setText("");
+                        imv.setImageResource(android.R.drawable.ic_menu_report_image);
                         Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
                             @Override
                             public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
@@ -105,6 +128,20 @@ public class ThongTinAdminFragment extends Fragment {
                             public void onComplete(@NonNull Task<Uri> task) {
                                 if (task.isSuccessful()) {
                                     Uri downloadUri = task.getResult();
+                                    String TenHinh=edtTenHinh.getText().toString();
+                                    String link=downloadUri.toString();
+                                    TestStorage HinhAnh=new TestStorage(TenHinh,link);
+
+                                    mData.child("Images").push().setValue(HinhAnh, new DatabaseReference.CompletionListener() {
+                                        @Override
+                                        public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                                            if(databaseError == null){
+                                                Toast.makeText(getActivity(), "Lưu link thành công", Toast.LENGTH_SHORT).show();
+                                            }else {
+                                                Toast.makeText(getActivity(), "Lỗi!!!", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
                                     Log.d("Link",downloadUri+"");
                                 } else {
                                     // Handle failures
@@ -112,17 +149,6 @@ public class ThongTinAdminFragment extends Fragment {
                                 }
                             }
                         });
-
-
-
-
-
-
-
-
-
-
-
                     }
                 });
             }
@@ -130,6 +156,36 @@ public class ThongTinAdminFragment extends Fragment {
 
         return view;
 
+    }
+    private void loadData(){
+        mData.child("Images").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                TestStorage hinhAnh=dataSnapshot.getValue(TestStorage.class);
+                dsh.add(new TestStorage(hinhAnh.tenHinh,hinhAnh.linkHinh));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/lenovo/duan1/model/TestStorage.java
@@ -0,0 +1,15 @@
+package com.example.lenovo.duan1.model;
+
+public class TestStorage {
+    public  String tenHinh;
+    public  String linkHinh;
+
+    public TestStorage() {
+
+    }
+
+    public TestStorage(String tenHinh, String linkHinh) {
+        this.tenHinh = tenHinh;
+        this.linkHinh = linkHinh;
+    }
+}
