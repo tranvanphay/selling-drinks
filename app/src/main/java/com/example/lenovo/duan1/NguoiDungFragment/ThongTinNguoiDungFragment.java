546588d6031972408a6c0b67969c8d796c8f92bd
4redevil1371 <35441105+4redevil1371@users.noreply.github.com>
2018-12-10 20:53:26 +0700

    đổi pass

--- app/src/main/java/com/example/lenovo/duan1/NguoiDungFragment/ThongTinNguoiDungFragment.java
+++ app/src/main/java/com/example/lenovo/duan1/NguoiDungFragment/ThongTinNguoiDungFragment.java
@@ -17,6 +17,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.lenovo.duan1.LoginActivity;
@@ -33,10 +34,13 @@ import com.google.firebase.auth.FirebaseUser;
  * A simple {@link Fragment} subclass.
  */
 public class ThongTinNguoiDungFragment extends Fragment {
-    Button bt_doiMatKhau,bt_dangXuat;
-    FirebaseAuth mAuthor=FirebaseAuth.getInstance();
-    FirebaseAuth.AuthStateListener mAuthListener;
+    Button bt_doiMatKhau, bt_dangXuat, btn_doipw;
+    EditText doipw;
 
+
+    FirebaseAuth mAuthor = FirebaseAuth.getInstance();
+    FirebaseAuth.AuthStateListener mAuthListener;
+    FirebaseAuth auth=FirebaseAuth.getInstance();
 
     public ThongTinNguoiDungFragment() {
         // Required empty public constructor
@@ -48,58 +52,59 @@ public class ThongTinNguoiDungFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_thong_tin_nguoi_dung, container, false);
-        bt_doiMatKhau=view.findViewById(R.id.bt_doiMatKhau);
-        bt_dangXuat=view.findViewById(R.id.bt_dangXuat);
-        bt_doiMatKhau.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
-                builder.setPositiveButton("Đồng ý!", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(final DialogInterface dialogInterface, int i) {
-                        String email=mAuthor.getCurrentUser().getEmail();
-                        mAuthor.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                Toast.makeText(getActivity(), "Vui lòng kiểm tra trong hộp thư Gmail", Toast.LENGTH_SHORT).show();
+        bt_doiMatKhau = view.findViewById(R.id.bt_doiMatKhau);
+        bt_dangXuat = view.findViewById(R.id.bt_dangXuat);
+        doipw = view.findViewById(R.id.ed_doipw);
 
+
+        btn_doipw = view.findViewById(R.id.btn_doipw);
+        btn_doipw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //// đổi pass. chỗ này là chỗ đổi pass. vứt vào đâu cũng dc
+                FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+                if(user!=null){
+                    user.updatePassword(doipw.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(getActivity(), "thanh cong", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getActivity(), ""+doipw.getText().toString(), Toast.LENGTH_SHORT).show();
                             }
-                        });
-                    }
-                });
-                builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                builder.setTitle("Đổi mật khẩu");
-                builder.setMessage("Bạn có thực sự muốn đổi mật khẩu?");
-                AlertDialog dialog=builder.create();
-                dialog.show();
+                            else
+                                Toast.makeText(getActivity(), "that bai", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                ///
             }
         });
+
+
+
         bt_dangXuat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //               mAuthor.removeAuthStateListener(mAuthListener);
-               mAuthor.signOut();
-               Intent i=new Intent(getActivity(),LoginActivity.class);
-               startActivity(i);
-               getActivity().finish();
+                mAuthor.signOut();
+                Intent i = new Intent(getActivity(), LoginActivity.class);
+                startActivity(i);
+                getActivity().finish();
 
             }
         });
-        mAuthListener= new FirebaseAuth.AuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
-                if(firebaseUser != null){
-                    String name=firebaseUser.getDisplayName();
-                    String email=firebaseUser.getEmail();
-                    Uri uri=firebaseUser.getPhotoUrl();
-                }else {
-                    Log.d("state","onStateChange");
+                FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                if (firebaseUser != null) {
+                    String name = firebaseUser.getDisplayName();
+                    String email = firebaseUser.getEmail();
+                    Uri uri = firebaseUser.getPhotoUrl();
+                } else {
+                    Log.d("state", "onStateChange");
                 }
             }
         };
@@ -107,5 +112,6 @@ public class ThongTinNguoiDungFragment extends Fragment {
 
         return view;
     }
+
 
 }
